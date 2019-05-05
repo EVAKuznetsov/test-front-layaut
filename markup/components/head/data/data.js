c45c7e709831e45204c0f8a28ec630e9e7fac76e
Evgeniy Kuznetsov <kuznetsov_2803@mail.ru>
2019-05-06 08:30:15 +1000

    layout

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,10 @@
+head: {
+    defaults: {
+        title: 'default title',
+        useSocialMetaTags: true
+    },
+    new: {
+        title: 'New title',
+        useSocialMetaTags: true
+    }
+}
